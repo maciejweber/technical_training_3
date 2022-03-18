09ec8bffe82c0a2d9568fe3c8e2e1f176d23456e
Maciej Weber <maciejwebber@gmail.com>
2022-03-18 18:05:48 +0100

    lab1

---
+++ MathService/WindowsApplicationFormsClient/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApplicationFormsClient
+{
+    public partial class Form1 : Form
+    {
+        private readonly MathServiceReference.MathServiceClient _client = new MathServiceReference.MathServiceClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+
+            int result = _client.Add(new MathServiceReference.MyNumbers() { Number1 = number1, Number2 = number2 });
+
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+
+            int result = _client.Subtract(new MathServiceReference.MyNumbers() { Number1 = number1, Number2 = number2 });
+
+            textBox3.Text = result.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
